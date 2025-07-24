f37da86b72e7e4fe66a140c2f1a3a550cd02f55f
BipinGosai <bipin.gosai321@gmail.com>
2025-07-24 14:18:08 +0545

    setup app, file and folder structure, routing and auth page s

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import AuthForms from '@/components/AuthForms'
+const page = () => {
+  return<AuthForms type="sign-up"/>
+}
+
+export default page
---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Home Page</div>
+  )
+}
+
+export default page
+
+
+
+
+
+
+
+
